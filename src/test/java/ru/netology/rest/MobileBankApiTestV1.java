253eabc9833c701bd8f74a7f9eb259a3ea11f880
Vadim Piskarev <woxadestroyer@gmail.com>
2022-08-12 22:29:08 +0300

    commit 3

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -4,23 +4,20 @@ import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+import static org.hamcrest.Matchers.equalTo;
 
 class MobileBankApiTestV1 {
     @Test
     void shouldReturnDemoAccounts() {
-        // Given - When - Then
-        // Предусловия
         given()
                 .baseUri("http://localhost:9999/api/v1")
-                // Выполняемые действия
                 .when()
                 .get("/demo/accounts")
-                // Проверки
-                // код теста
                 .then()
-                .statusCode(401)
-                // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
+                .statusCode(200)
                 .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+                .body("[0].currency", equalTo("RUB"))
+                .body("[1].currency", equalTo("USD"))
         ;
     }
 }
