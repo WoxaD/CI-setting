7e3af812e7ae0b340c1e485ddef533b364edeb21
Vadim Piskarev <woxadestroyer@gmail.com>
2022-08-12 22:36:22 +0300

    fixed commit

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -16,9 +16,9 @@ class MobileBankApiTestV1 {
                 .then()
                 .statusCode(200)
                 .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
-                .body("[0].currency", equalTo("RUR"))
+                .body("[0].currency", equalTo("RUB"))
                 .body("[1].currency", equalTo("USD"))
-                .body("[2].currency", equalTo("RUR"))
+                .body("[2].currency", equalTo("RUB"))
         ;
     }
 }
